6c4fefd32882803dbcbb9661c155d891f42b16d2
Andyeeeee <10514f074@gmail.com>
2024-06-18 13:13:29 +0800

    套件問題?

--- src/layouts/FrontLayout.vue
+++ src/layouts/FrontLayout.vue
@@ -3,7 +3,32 @@
   <v-main>
     <router-view />
   </v-main>
+  <v-layout class="overflow-visible" style="height: 56px;">
+    <v-bottom-navigation color="primary">
+      <v-btn to="/">
+        <v-icon>mdi-history</v-icon>
 
+        首頁
+      </v-btn>
+
+      <v-btn to="movie">
+        <v-icon>mdi-heart</v-icon>
+
+        電影時刻
+      </v-btn>
+
+      <v-btn to="Store">
+        <v-icon>mdi-map-marker</v-icon>
+
+        <span>點數兌換</span>
+      </v-btn>
+      <v-btn to="login">
+        <v-icon>mdi-map-marker</v-icon>
+
+        <span>會員登入</span>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-layout>
 </template>
 
 <script setup>
