8e3908d9760ca2716bed90f0178c031336946bdb
Andyeeeee <10514f074@gmail.com>
2024-06-18 12:11:04 +0800

    asd

--- src/router/index.js
+++ src/router/index.js
@@ -4,15 +4,19 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    component: () => import('@/layouts/FrontLayout.vue'),
+    component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
         path: '',
         name: 'Home',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/front/HomeView.vue'),
         meta: {
           title: '首頁'
-        },
+        }
+
       },
     ],
   },
