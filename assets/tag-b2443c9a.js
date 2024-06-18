7ac79ace7b773452b27200ff2d6007110b2f7b97
Andyeeeee <10514f074@gmail.com>
2024-06-18 09:16:45 +0000

    Deploying to gh-pages from @ Andyeeeee/only@7d7d6bd970625c15042a3be871dd77cc48f2a369 🚀

---
+++ assets/tag-b2443c9a.js
@@ -0,0 +1,1 @@
+import{p as t}from"./index-e3bd6a7f.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
--- assets/tag-ea5f5f33.js
+++
@@ -1,1 +0,0 @@
-import{p as t}from"./index-5778ed49.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
