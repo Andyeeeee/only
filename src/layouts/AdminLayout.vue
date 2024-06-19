9c0c539c8078f8ee90cfab9048d44870003ff29c
Andyeeeee <10514f074@gmail.com>
2024-06-19 16:05:13 +0800

    d

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -1,6 +1,55 @@
 <template>
-  adminLayout
-  <router-view style="margin-bottom: 56px;"></router-view>
+
+  <section class="layout-title-pic text-center ">the-word
+  </section>
+  <v-main style="margin-bottom: 56px;">
+    <router-view />
+  </v-main>
+
+  <v-layout class="overflow-visible" style="height: 56px;position: fixed; width: 100%;bottom: 0%;">
+    <v-bottom-navigation v-model="value" color="primary" active>
+      <v-btn to="/">
+        <v-icon>mdi-home</v-icon>
+        首頁
+      </v-btn>
+
+      <v-btn to="movie">
+        <v-icon>mdi-movie</v-icon>
+
+        電影
+      </v-btn>
+
+      <v-btn to="store">
+        <v-icon>mdi-store-alert-outline</v-icon>
+
+        <span>設施</span>
+      </v-btn>
+
+
+      <v-btn to="login">
+        <v-icon>mdi-account</v-icon>
+
+        <span>登入</span>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-layout>
 
 </template>
-<script setup></script>
+
+<script setup>
+//
+</script>
+
+<style>
+ul,
+li {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.navbar {
+  display: flex;
+  justify-content: center;
+}
+</style>
