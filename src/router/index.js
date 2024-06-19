1596290a767e8d1a6012c6b013745e2641c08635
Andyeeeee <10514f074@gmail.com>
2024-06-19 14:46:29 +0800

    only login

--- src/router/index.js
+++ src/router/index.js
@@ -14,47 +14,47 @@ const routes = [
           title: '登入'
         },
       },
-      {
-        path: '',
-        name: 'Home',
-        component: () => import(/* webpackChunkName: "home" */ '@/views/front/HomeView.vue'),
-        meta: {
-          title: '首頁'
-        }
+      // {
+      //   path: '',
+      //   name: 'Home',
+      //   component: () => import(/* webpackChunkName: "home" */ '@/views/front/HomeView.vue'),
+      //   meta: {
+      //     title: '首頁'
+      //   }
 
-      },
-      {
-        path: 'movie',
-        name: 'Movie',
-        component: () => import(/* webpackChunkName: "movie" */ '@/views/front/Movie.vue'),
-        meta: {
-          title: '電影時刻'
-        }
-      },
-      {
-        path: 'store',
-        name: 'Store',
-        component: () => import(/* webpackChunkName: "store" */ '@/views/front/Store.vue'),
-        meta: {
-          title: '點數兌換'
-        }
-      },
-      {
-        path: 'storee',
-        name: 'Storee',
-        component: () => import(/* webpackChunkName: "storee" */ '@/views/front/Storee.vue'),
-        meta: {
-          title: '點數兌換e'
-        }
-      },
-      {
-        path: 'loginv',
-        name: 'Loginv',
-        component: () => import(/* webpackChunkName: "loginv" */ '@/views/front/Loginv.vue'),
-        meta: {
-          title: '點換'
-        }
-      },
+      // },
+      // {
+      //   path: 'movie',
+      //   name: 'Movie',
+      //   component: () => import(/* webpackChunkName: "movie" */ '@/views/front/Movie.vue'),
+      //   meta: {
+      //     title: '電影時刻'
+      //   }
+      // },
+      // {
+      //   path: 'store',
+      //   name: 'Store',
+      //   component: () => import(/* webpackChunkName: "store" */ '@/views/front/Store.vue'),
+      //   meta: {
+      //     title: '點數兌換'
+      //   }
+      // },
+      // {
+      //   path: 'storee',
+      //   name: 'Storee',
+      //   component: () => import(/* webpackChunkName: "storee" */ '@/views/front/Storee.vue'),
+      //   meta: {
+      //     title: '點數兌換e'
+      //   }
+      // },
+      // {
+      //   path: 'loginv',
+      //   name: 'Loginv',
+      //   component: () => import(/* webpackChunkName: "loginv" */ '@/views/front/Loginv.vue'),
+      //   meta: {
+      //     title: '點換'
+      //   }
+      // },
     ],
   },
   // {
