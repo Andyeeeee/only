18d935631ed4852c727847c966b9872cf0ee13bf
Andyeeeee <10514f074@gmail.com>
2024-06-19 15:51:20 +0800

    只留layout

--- src/router/index.js
+++ src/router/index.js
@@ -45,39 +45,17 @@ const routes = [
   {
     path: '/admin',
     component: () => import('@/layouts/AdminLayout.vue'),
-    children: [
-      {
-        path: '',
-        name: 'AdminHome',
-        component: () => import(/* webpackChunkName: "admin-home" */ '@/views/admin/HomeView.vue'),
-        meta: {
-          title: '購物網 | 管理',
-          login: true,
-          admin: true
-        }
-      },
-      {
-        path: 'products',
-        name: 'AdminProducts',
-        component: () => import(/* webpackChunkName: "admin-products" */ '@/views/admin/ProductsView.vue'),
-        meta: {
-          title: '購物網 | 商品管理',
-          login: true,
-          admin: true
-        }
-      },
-      {
-        path: 'orders',
-        name: 'AdminOrders',
-        component: () => import(/* webpackChunkName: "admin-orders" */ '@/views/admin/OrdersView.vue'),
-        meta: {
-          title: '購物網 | 訂單管理',
-          login: true,
-          admin: true
-        }
-      }
-    ]
-  }
+
+  },
+  //     {
+  //       path: 'adminproducts',
+  //       name: 'AdminProducts',
+  //       component: () => import(/* webpackChunkName: "admin-products" */ '@/views/back/adminproducts.vue'),
+  //       meta: {
+  //         title: '商品管理'
+  //       }
+  //     },
+
 ]
 
 
